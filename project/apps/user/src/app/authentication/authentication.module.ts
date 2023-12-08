db619c271363a9d35ec9b6158ec812dfb631954e
Maks Tkachenko <tkachenkonino1996@gmail.com>
2023-12-08 13:36:45 +0300

    feat(*): add controller/service for auth module

--- project/apps/user/src/app/authentication/authentication.module.ts
+++ project/apps/user/src/app/authentication/authentication.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { AuthenticationService } from './authentication.service';
+import { AuthenticationController } from './authentication.controller';
 
 @Module({
   imports: [],
-  controllers: [],
-  providers: [],
+  controllers: [AuthenticationController],
+  providers: [AuthenticationService],
 })
 export class AuthenticationModule {}
